204cc1230b57f065b156d8f9ce8301f824ae14fb
MiguelBonilla-sys <109702313+MiguelBonilla-sys@users.noreply.github.com>
2023-10-28 15:34:33 -0500

    Primera Entrega

---
+++ app/src/main/java/com/example/ecorecicla/categoryPapelYCarton.java
@@ -0,0 +1,14 @@
+package com.example.ecorecicla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class categoryPapelYCarton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_papel_ycarton);
+    }
+}
