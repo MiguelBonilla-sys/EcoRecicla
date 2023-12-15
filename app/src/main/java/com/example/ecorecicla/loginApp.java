e87ff78ec39e80f8327e0804b8d2182ed9d75a83
MiguelBonilla-sys <109702313+MiguelBonilla-sys@users.noreply.github.com>
2023-12-14 21:40:12 -0500

    14-12-2023Act1-FINAL

--- app/src/main/java/com/example/ecorecicla/loginApp.java
+++ app/src/main/java/com/example/ecorecicla/loginApp.java
@@ -1,6 +1,7 @@
 package com.example.ecorecicla;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,15 @@ public class loginApp extends AppCompatActivity {
             String password=userData[2];
 
             if(email.equals(user) || Name.equals(user) && password.equals(pass)){
+                // Guardar los datos del usuario en SharedPreferences después de un inicio de sesión exitoso
+                SharedPreferences sharedPreferences = getSharedPreferences("UserPreferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", true);
+                editor.putString("name", Name);
+                editor.putString("email", email);
+                editor.putString("password", password);
+                editor.apply();
+
                 return true;
             }
         }
